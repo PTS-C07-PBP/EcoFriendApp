87a3b0695f344b5fe7fceda267e53e88f6204a2c
kayleerudaina <kayleerudaina@gmail.com>
2022-12-08 22:12:55 +0700

    add register page

--- ecofriend/lib/pages/user/register.dart
+++ ecofriend/lib/pages/user/register.dart
@@ -48,12 +48,15 @@ class _RegisterPageState extends State<RegisterPage> {
   String _username = "";
   String _firstname = "";
   String _lastname = "";
+  String _password1 = "";
+  String _password2 = "";
   String? _role = 'EcoUser';
   final List<String> _listRole = [
     'Admin',
     'EcoUser',
   ];
-  String _description = '';
+
+  String statusMessage = "";
 
   @override
   Widget build(BuildContext context) {
@@ -72,127 +75,202 @@ class _RegisterPageState extends State<RegisterPage> {
       drawer: const CustomDrawer(),
       body: Form(
         key: _registerFormKey,
-        child: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(20.0),
-            height: MediaQuery.of(context).size.height -
-                AppBar().preferredSize.height,
-            child: Column(
-              children: [
-                Padding(
-                  // Input Title
-                  padding: const EdgeInsets.all(10.0),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: const Color(0xffffffff),
-                      labelText: "Title",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0),
-                      ),
-                    ),
-                    style: const TextStyle(
-                      fontSize: 14.0,
-                    ),
-
-                    // Update variabel title
-                    onChanged: (String? value) {
-                      setState(() {
-                        _username = value!;
-                      });
-                    },
-                    onSaved: (String? value) {
-                      setState(() {
-                        _username = value!;
-                      });
-                    },
-
-                    // Validator sebagai validasi form
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Title cannot be empty!';
-                      }
-                      return null;
-                    },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(90.0),
                   ),
+                  labelText: 'Username',
                 ),
-                
-                Padding(
-                  // Input description
-                  padding: const EdgeInsets.all(10.0),
-                  child: TextFormField(
-                    maxLines: 6,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: const Color(0xffffffff),
-                      labelText: "Description",
-                      alignLabelWithHint: true,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0),
-                      ),
-                    ),
-                    style: const TextStyle(
-                      fontSize: 14.0,
-                    ),
-
-                    // Update variabel description
-                    onChanged: (String? value) {
-                      setState(() {
-                        _description = value!;
-                      });
-                    },
-                    onSaved: (String? value) {
-                      setState(() {
-                        _description = value!;
-                      });
-                    },
-
-                    // Validator sebagai validasi form
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Description cannot be empty!';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                const Spacer(),
-                SizedBox(
-                    width: 150,
-                    height: 50,
-                    child: TextButton(
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(const Color(0xff00fc97)),
-                      ),
-                      onPressed: () {
-                        if (_registerFormKey.currentState!.validate()) {
-                          postArticle(request);
-                          print(_username);
-                          print(_role);
-                          print(_description);
-                          Navigator.pop(context);
-                        }
-                      },
-                      child: const Text(
-                        "Register New EcoUser",
-                        style: TextStyle(color: Colors.black, fontSize: 14),
-                      ),
-                    )),
-              ],
+                onChanged: (String? value) {
+                    setState((){
+                        _username = value!;
+                    });
+                },
+                validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                        return 'Username is required!';
+                    }
+                    return null;
+                },
+              ),
             ),
-          ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(90.0),
+                  ),
+                  labelText: 'First Name',
+                ),
+                onChanged: (String? value) {
+                    setState((){
+                        _firstname = value!;
+                    });
+                },
+                validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                        return 'Username is required!';
+                    }
+                    return null;
+                },
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(90.0),
+                  ),
+                  labelText: 'Last Name',
+                ),
+                onChanged: (String? value) {
+                    setState((){
+                        _firstname = value!;
+                    });
+                },
+                validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                        return 'Username is required!';
+                    }
+                    return null;
+                },
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(90.0),
+                  ),
+                  labelText: 'Password',
+                ),
+
+                onChanged: (String? value) {
+                    setState((){
+                        _password1 = value!;
+                    });
+                },
+                validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                        return 'Password is required!';
+                    }
+                    return null;
+                },
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(90.0),
+                  ),
+                  labelText: 'Password Confirmation',
+                ),
+
+                onChanged: (String? value) {
+                    setState((){
+                        _password2 = value!;
+                    });
+                },
+                validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                        return 'Password is required!';
+                    }
+
+                    if (value != _password1) {
+                        return 'Password not matched!';
+                    }
+                    return null;
+                },
+              ),
+            ),
+            Container(
+                height: 80,
+                padding: const EdgeInsets.all(20),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(50),
+                  ),
+                  child: const Text('Log In'),
+                  onPressed: () async {
+                    if (_registerFormKey.currentState!.validate()) {
+                      final response = await request.login("https://ecofriend.up.railway.app/authentication/login/", {
+                                                    'username': _username,
+                                                    'password': _password1,
+                      }).then((value) {
+                        final newValue = new Map<String, dynamic>.from(value);
+                        print(newValue['message']);
+
+                        setState(() {
+                          if (newValue['message'].toString() ==
+                              "Failed to Login, check your email/password.") {
+                            statusMessage = "Invalid username/password!";
+                          } else {
+                            statusMessage = newValue['message'].toString();
+                          }
+                        });
+                      });
+                      if (request.loggedIn) {
+                        // Code here will run if the login succeeded.
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          content: Text("Login success!"),
+                        ));
+                          Navigator.pushNamed(context, '/');
+                      }
+                      // else {
+                      //   // Code here will run if the login failed (wrong username/password).
+                      //   showDialog(context: context, 
+                      //   builder: (context) {
+                      //     return Dialog (
+                      //       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                      //       elevation: 10,
+                      //       child: Container(
+                      //         child: ListView(
+                      //           padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                      //           shrinkWrap: true,
+                      //           children: <Widget>[
+                      //             const Center(
+                      //                 child: Text(
+                      //                     'Username or Password is wrong',
+                      //                     style: TextStyle(
+                      //                         fontSize: 24,
+                      //                         fontWeight: FontWeight.bold
+                      //                     ),
+                      //                 )
+                      //             ),
+                      //             const SizedBox(height: 20),
+                      //             TextButton(
+                      //                 onPressed: () {
+                      //                     Navigator.pop(context);
+                      //                 },
+                      //                 child: const Text('Try again'),
+                      //             )
+                      //           ]
+                      //         )
+                      //       )
+                      //       );
+                      //   });
+                      // }
+                    }
+                  }
+                )),
+            Text(statusMessage),
+          ],
         ),
       ),
     );
-  }
-
-  void postArticle(CookieRequest request) async {
-    //TODO: Tunggu authenticate
-    var url = Uri.parse('https://ecofriend.up.railway.app/news/add/');
-
-    var response = await request.post(
-        'https://ecofriend.up.railway.app/news/add/',
-        jsonEncode({'title': _username, 'region': _role, 'description': _description}));
   }
 }
