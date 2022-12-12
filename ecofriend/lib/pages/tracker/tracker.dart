7710190b249a04de14567c67a630a59bb752b2d7
Rahma Adinda Putri <putrirahmaadinda@gmail.com>
2022-12-12 16:53:32 +0700

    fix add data

--- ecofriend/lib/pages/tracker/tracker.dart
+++ ecofriend/lib/pages/tracker/tracker.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 import '../custom_drawer.dart';
-import '../user/login.dart';
 
 import 'add_footprint.dart';
 
